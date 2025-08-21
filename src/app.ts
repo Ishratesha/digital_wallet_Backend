001da4504d874d58ebabbcff01dda3773ae56ccb
Ishrat Jahan Esha <84029777+Ishratesha@users.noreply.github.com>
2025-08-21 18:37:45 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { AuthRoutes } from './app/modules/auth/auth.route';
 import transactionRoutes from './app/modules/transaction/transaction.route';
 import adminRoutes from './app/modules/admin/admin.route';
 
-//import cors from 'cors';
+import cors from 'cors';
 //import morgan from 'morgan';
 //import globalErrorHandler from './app/middlewares/globalErrorHandler';
 //import notFoundHandler from './app/middlewares/notFoundHandler';
@@ -13,6 +13,10 @@ import adminRoutes from './app/modules/admin/admin.route';
 const app: Application = express();
 
 // Middlewares
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
